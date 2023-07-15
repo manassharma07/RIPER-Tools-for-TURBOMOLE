eca7269b73f2f0862766e9c62154af7dfcfaaf1d
manas sharma <mikeatthetop@gmail.com>
2023-07-15 10:43:52 +0200

    fix cif downloading buttons

--- pages/Materials_Project_to_RIPER.py
+++ pages/Materials_Project_to_RIPER.py
@@ -196,11 +196,11 @@ if docs is not None:
     # Download CIF files
     st.subheader("Download CIF Files")
     col1, col2 = st.columns(2)
-    convert_to_cif(primitive_structure, read_file("primitive_unit_cell.cif"), "primitive_unit_cell.cif")
-    col1.download_button('Download Primitive Unit Cell CIF', "primitive_unit_cell.cif", file_name='primitive_unit_cell.cif', key='primitive_cif_button')
+    convert_to_cif(primitive_structure, "primitive_unit_cell.cif")
+    col1.download_button('Download Primitive Unit Cell CIF', read_file("primitive_unit_cell.cif"), "primitive_unit_cell.cif", file_name='primitive_unit_cell.cif', key='primitive_cif_button')
 
-    convert_to_cif(conventional_structure, read_file("conventional_unit_cell.cif"), "conventional_unit_cell.cif")
-    col2.download_button('Download Conventional Unit Cell CIF', "conventional_unit_cell.cif", file_name='conventional_unit_cell.cif', key='conventional_cif_button')
+    convert_to_cif(conventional_structure, "conventional_unit_cell.cif")
+    col2.download_button('Download Conventional Unit Cell CIF', read_file("conventional_unit_cell.cif"), "conventional_unit_cell.cif", file_name='conventional_unit_cell.cif', key='conventional_cif_button')
 
     # Get TURBOMOLE (RIPER) Coord file and Control file contents
     st.subheader("RIPER Files")
