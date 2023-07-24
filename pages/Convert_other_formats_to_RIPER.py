be9fc981c8e122b0e5788bf41465a7365475f489
Lexachoc <hoo6103yyy@gmail.com>
2023-07-24 23:00:46 +0200

    1. Add conversion format from CAR to TURBOMOLE 2. Update the requirements.txt 3. Change CifParser.from_string to CifParser.from_str

--- pages/Convert_other_formats_to_RIPER.py
+++ pages/Convert_other_formats_to_RIPER.py
@@ -14,12 +14,13 @@ from ase.io.espresso import read_espresso_in
 from ase.io.cif import read_cif
 from ase.io import read
 from pymatgen.io.ase import AseAtomsAdaptor
+from ase.io.dmol import read_dmol_car
 
 # Set page config
-st.set_page_config(page_title='CIF/POSCAR/XYZ ➡️ RIPER', layout='wide', page_icon="⚛️",
-menu_items={
-         'About': "A web app to help you with DFT related calculations using the RIPER module of [TURBOMOLE](https://www.turbomole.org/)"
-     })
+st.set_page_config(page_title='CIF/XYZ/CAR/POSCAR/PWSCF ➡️ RIPER', layout='wide', page_icon="⚛️",
+                   menu_items={
+                       'About': "A web app to help you with DFT related calculations using the RIPER module of [TURBOMOLE](https://www.turbomole.org/)"
+                   })
 
 # Sidebar stuff
 st.sidebar.write('# About')
@@ -32,18 +33,21 @@ st.sidebar.write('* [PyMatgen](https://pymatgen.org/) for Periodic Structure Rep
 st.sidebar.write('* [PubChempy](https://pypi.org/project/PubChemPy/1.0/) for Accessing the PubChem Database')
 st.sidebar.write('* [MP-API](https://pypi.org/project/mp-api/) for Accessing the Materials Project Database')
 
+
 # Function to convert atomic coordinates to Bohr units
 def convert_to_bohr(structure):
     coords = [(site.coords[0], site.coords[1], site.coords[2], site.species_string) for site in structure.sites]
     return [(x * 1.8897259886, y * 1.8897259886, z * 1.8897259886, element.lower()) for x, y, z, element in coords]
 
+
 # Function to generate coordinate text
 def generate_coord_text(coords_bohr):
     coord_text = "$coord\n"
     for coord in coords_bohr:
-        coord_text += f"    {coord[0]:.8f}   {coord[1]:.8f}   {coord[2]:.8f}    {coord[3]}\n"
+        coord_text += f"{coord[0]:>20.14f}  {coord[1]:>20.14f}  {coord[2]:>20.14f}  {coord[3]:<2s}\n"
     coord_text += "$end"
     return coord_text
+
 
 # Function to generate lattice parameter text
 def generate_lattice_text(structure):
@@ -56,25 +60,27 @@ def generate_lattice_text(structure):
     lattice_text += "    nkpoints 1 1 1 # Gamma point calculation"
     return lattice_text
 
+
 # Function to convert a structure to CIF
 def convert_to_cif(structure, filename):
     cif_writer = CifWriter(structure)
     cif_writer.write_file(filename)
 
+
 # Function to visualize the structure using py3Dmol
 def visualize_structure(structure, html_file_name='viz.html'):
-    spin = st.checkbox('Spin', value = False, key='key'+html_file_name)
+    spin = st.checkbox('Spin', value=False, key='key' + html_file_name)
     view = py3Dmol.view(width=500, height=400)
     cif_for_visualization = structure.to(fmt="cif")
     view.addModel(cif_for_visualization, 'cif')
     # view.setStyle({'stick': {'radius': 0.2}})
-    view.setStyle({'sphere':{'colorscheme':'Jmol','scale':0.3},
-                    'stick':{'colorscheme':'Jmol', 'radius':0.2}})
+    view.setStyle({'sphere': {'colorscheme': 'Jmol', 'scale': 0.3},
+                   'stick': {'colorscheme': 'Jmol', 'radius': 0.2}})
     view.addUnitCell()
     view.zoomTo()
     view.spin(spin)
-    view.setClickable({'clickable':'true'});
-    view.enableContextMenu({'contextMenuEnabled':'true'})
+    view.setClickable({'clickable': 'true'})
+    view.enableContextMenu({'contextMenuEnabled': 'true'})
     view.show()
     view.render()
     # view.png()
@@ -85,23 +91,24 @@ def visualize_structure(structure, html_file_name='viz.html'):
     f.close()
 
     HtmlFile = open(html_file_name, 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    components.html(source_code, height = 300, width=900)
+    source_code = HtmlFile.read()
+    components.html(source_code, height=300, width=900)
     HtmlFile.close()
+
 
 # Function to visualize the structure using py3Dmol
 def visualize_molecule(structure, html_file_name='viz.html'):
-    spin = st.checkbox('Spin', value = False, key='key'+html_file_name)
+    spin = st.checkbox('Spin', value=False, key='key' + html_file_name)
     view = py3Dmol.view(width=500, height=400)
     cif_for_visualization = structure.to(fmt="xyz")
     view.addModel(cif_for_visualization, 'xyz')
     # view.setStyle({'stick': {'radius': 0.2}})
-    view.setStyle({'sphere':{'colorscheme':'Jmol','scale':0.3},
-                    'stick':{'colorscheme':'Jmol', 'radius':0.2}})
+    view.setStyle({'sphere': {'colorscheme': 'Jmol', 'scale': 0.3},
+                   'stick': {'colorscheme': 'Jmol', 'radius': 0.2}})
     view.zoomTo()
     view.spin(spin)
-    view.setClickable({'clickable':'true'});
-    view.enableContextMenu({'contextMenuEnabled':'true'})
+    view.setClickable({'clickable': 'true'})
+    view.enableContextMenu({'contextMenuEnabled': 'true'})
     view.show()
     view.render()
     # view.png()
@@ -112,17 +119,17 @@ def visualize_molecule(structure, html_file_name='viz.html'):
     f.close()
 
     HtmlFile = open(html_file_name, 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    components.html(source_code, height = 300, width=900)
+    source_code = HtmlFile.read()
+    components.html(source_code, height=300, width=900)
     HtmlFile.close()
-    
+
 
 # Function to display structure information
 def display_structure_info(structure):
     st.subheader("Structure Information")
     st.write("Formula: ", structure.composition.reduced_formula)
 
-    #Display lattice parameters
+    # Display lattice parameters
     a, b, c = structure.lattice.abc
     alpha, beta, gamma = structure.lattice.angles
 
@@ -144,7 +151,7 @@ def display_structure_info(structure):
     # Create a list of atomic coordinates
     with st.expander("Atomic Coordinates", expanded=False):
         coord_type = st.selectbox('Coordinate type', ['Cartesian', 'Fractional/Crystal'])
-        if coord_type=='Cartesian':
+        if coord_type == 'Cartesian':
             atomic_coords = []
             for site in structure.sites:
                 atomic_coords.append([site.species_string] + list(site.coords))
@@ -152,38 +159,42 @@ def display_structure_info(structure):
             atomic_coords = []
             for site in structure.sites:
                 atomic_coords.append([site.species_string] + list(site.frac_coords))
-        
+
         # Create a Pandas DataFrame from the atomic coordinates list
         df_coords = pd.DataFrame(atomic_coords, columns=["Element", "X", "Y", "Z"])
 
-    
         # Display the atomic coordinates as a table
         # st.write("Atomic Coordinates:")
         st.table(df_coords)
 
+
 def parse_cif_pymatgen(contents):
     # Parse the CIF file using pymatgen
-    cif_parser = CifParser.from_string(contents)
+    cif_parser = CifParser.from_str(contents)
     structure = cif_parser.get_structures(primitive=False)[0]  # Assuming there's only one structure in the CIF file
     return structure
 
+
 def parse_xyz(contents):
     # Parse the XYZ file using pymatgen
-    xyz_parser = XYZ.from_string(contents)
+    xyz_parser = XYZ.from_str(contents)
     structure = xyz_parser.molecule  # Assuming it's a molecule XYZ file
     return structure
 
+
 def parse_poscar(contents):
     # Parse the POSCAR file using pymatgen
-    poscar_parser = Poscar.from_string(contents)
+    poscar_parser = Poscar.from_str(contents)
     structure = poscar_parser.structure
     return structure
 
+
 def parse_quantum_espresso(contents):
     # Parse the POSCAR file using pymatgen
-    qe_parser = PWInput.from_string(contents)
+    qe_parser = PWInput.from_str(contents)
     structure = qe_parser.structure
     return structure
+
 
 def parse_qe_ase(stringio):
     # Read QE input file
@@ -193,6 +204,7 @@ def parse_qe_ase(stringio):
     structure = AseAtomsAdaptor().get_structure(atoms)
     return structure
 
+
 def parse_cif_ase(stringio):
     # Read CIF
     atoms = read(stringio, format="cif")
@@ -201,27 +213,39 @@ def parse_cif_ase(stringio):
     structure = AseAtomsAdaptor().get_structure(atoms)
     return structure
 
+def parse_car_ase(stringio):
+    # Read CAR
+    atoms = read_dmol_car(stringio)
+    # Convert ASE Atoms to pymatgen Structure
+    structure = AseAtomsAdaptor().get_structure(atoms)
+    return structure
+
+
+
 # return filecontents
 def read_file(filename):
     with open(filename, 'r') as file:
         return file.read()
 
+
 # Streamlit app
-st.write('# CIF/XYZ/POSCAR/PWSCF ➡️ RIPER')
-st.write("#### Get atomic coordinates and cell parameters for RIPER (TURBOMOLE) from a CIF/POSCAR or XYZ (only atomic coordinates)")
+st.write('# CIF/XYZ/CAR/POSCAR/PWSCF ➡️ RIPER')
+st.write(
+    "#### Get atomic coordinates and cell parameters for RIPER (TURBOMOLE) from a CIF/POSCAR or XYZ (only atomic coordinates)")  # TODO
 
 st.write("Please select the file format")
 
 # Select file format
-file_format = st.selectbox("Select file format", ("CIF", "XYZ", "POSCAR", "Quantum ESPRESSO (PWSCF)"))
+file_format = st.selectbox("Select file format",
+                           ("CIF", "XYZ", "CAR (Materials Studio)", "POSCAR", "Quantum ESPRESSO (PWSCF)"))
 
-if file_format=='CIF':
+if file_format == 'CIF':
     cif_parser_options = ['ASE', 'PYMATGEN']
     selected_cif_parser = st.selectbox("Select a parser for CIFs", cif_parser_options)
 
-
 st.write('You can either paste the source file contents below or upload the source file')
-contents = st.text_area(label='Enter the contents of the source file here', value = '', placeholder = 'Put your text here', height=400, key = 'input_text_area')
+contents = st.text_area(label='Enter the contents of the source file here', value='', placeholder='Put your text here',
+                        height=400, key='input_text_area')
 
 # Create a file uploader widget
 file = st.file_uploader("or Upload the file")
@@ -238,18 +262,22 @@ if file is not None:
     # To read file as string:
     contents = stringio.read()
 
-if contents!='':
+if contents != '':
 
     # Parse the file based on the selected format
     if file_format == "CIF":
-        if selected_cif_parser=='PYMATGEN':
+        if selected_cif_parser == 'PYMATGEN':
             structure = parse_cif_pymatgen(contents)
-        elif selected_cif_parser=='ASE':
+        elif selected_cif_parser == 'ASE':
             # Create a StringIO object
             stringio_obj_cif = StringIO(contents)
             structure = parse_cif_ase(stringio_obj_cif)
     elif file_format == "XYZ":
         structure = parse_xyz(contents)
+    elif file_format == "CAR (Materials Studio)":
+        # structure = parse_car(contents)
+        stringio_obj_car = StringIO(contents)
+        structure = parse_car_ase(stringio_obj_car)
     elif file_format == "POSCAR":
         structure = parse_poscar(contents)
     elif file_format == "Quantum ESPRESSO (PWSCF)":
@@ -266,25 +294,24 @@ if contents!='':
     st.write("Successfully parsed file!")
     # st.write("Pymatgen Structure:")
     # st.write(structure)
-    
+
     # Display structure information
-    if not file_format=='XYZ':
+    if not file_format == 'XYZ':
         display_structure_info(structure)
     else:
         st.subheader("Atomic Coordinates")
         # Create a dataframe with atomic symbols and atomic coordinates
 
     # Visualize the structure
-    if not file_format=='XYZ':
+    if not file_format == 'XYZ':
         visualize_structure(structure, "viz1.html")
     else:
         visualize_molecule(structure, "viz1.html")
 
     # Download CIF files
-    if not file_format=='XYZ':
+    if not file_format == 'XYZ':
         st.subheader("Download CIF Files")
-        
-        
+
         convert_to_cif(structure, "structure.cif")
         st.download_button('Download CIF', data=read_file("structure.cif"), file_name='structure.cif', key='cif_button')
 
@@ -295,9 +322,9 @@ if contents!='':
 
     # Generate the coordinate text
     coords_text = generate_coord_text(coords_bohr)
-    
+
     # Generate the lattice parameter text
-    if not file_format=='XYZ':
+    if not file_format == 'XYZ':
         lattice_text = generate_lattice_text(structure)
 
     # Create two columns for text boxes
@@ -305,16 +332,16 @@ if contents!='':
 
     # Display the coordinate text in the first column
     with col1:
-        st.text_area("Coord file contents (Cartesian coordinates in Bohr)", value=coords_text, height=300, key='coords_text')
+        st.text_area("Coord file contents (Cartesian coordinates in Bohr)", value=coords_text, height=300,
+                     key='coords_text')
         st.download_button('Download coord file', coords_text, file_name='coord', key='control_text')
 
-    if not file_format=='XYZ':
+    if not file_format == 'XYZ':
         # Display the lattice parameters text in the second column
         with col2:
             st.text_area("Add the following to your control file", value=lattice_text, height=300)
 
-
-    if not file_format=='XYZ':
+    if not file_format == 'XYZ':
         # Create supercells
         # with st.expander("Model Supercell", expanded=False):
         st.subheader('Model Supercell')
@@ -346,10 +373,12 @@ if contents!='':
 
         # Display the coordinate text in the first column
         with col1:
-            st.text_area("Coord file contents (Cartesian coordinates in Bohr)", value=coords_text_super, height=300, key='supercell_text_coord')
+            st.text_area("Coord file contents (Cartesian coordinates in Bohr)", value=coords_text_super, height=300,
+                         key='supercell_text_coord')
+
             st.download_button('Download coord file', coords_text_super, file_name='coord')
 
         # Display the lattice parameters text in the second column
         with col2:
-            st.text_area("Add the following to your control file", value=lattice_text_super, height=300, key='supercell_text_control')
-
+            st.text_area("Add the following to your control file", value=lattice_text_super, height=300,
+                         key='supercell_text_control')
