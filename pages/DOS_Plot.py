38c9d4d4e8ab6b8efb6c6d084121640d21d03568
manas sharma <mikeatthetop@gmail.com>
2023-08-16 22:52:03 +0200

    minor changes to DOS plotter

--- pages/DOS_Plot.py
+++ pages/DOS_Plot.py
@@ -2,6 +2,30 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+# Set page config
+st.set_page_config(page_title='Convergence Tips for RIPER', layout='wide', page_icon="⚛️",
+menu_items={
+         'About': "A web app to help you with DFT related calculations using the RIPER module of [TURBOMOLE](https://www.turbomole.org/)"
+     })
+
+# Sidebar stuff
+st.sidebar.write('# About')
+st.sidebar.write(' Originally Made By [Manas Sharma](https://manas.bragitoff.com)')
+st.sidebar.write(' In the group of [Prof. Dr. Marek Sierka](https://cmsg.uni-jena.de)')
+st.sidebar.write('### *Powered by*')
+st.sidebar.write('* [Py3Dmol](https://3dmol.csb.pitt.edu/) for Chemical System Visualizations')
+st.sidebar.write('* [Streamlit](https://streamlit.io/) for making of the Web App')
+st.sidebar.write('* [PyMatgen](https://pymatgen.org/) for Periodic Structure Representations')
+st.sidebar.write('* [PubChempy](https://pypi.org/project/PubChemPy/1.0/) for Accessing the PubChem Database')
+st.sidebar.write('* [MP-API](https://pypi.org/project/mp-api/) for Accessing the Materials Project Database')
+st.sidebar.write('* [ASE](https://wiki.fysik.dtu.dk/ase/) for File Format Conversions')
+st.sidebar.write('### *Contributors*')
+st.sidebar.write('[Ya-Fan Chen ](https://github.com/Lexachoc)')
+st.sidebar.write('### *Source Code*')
+st.sidebar.write('[GitHub Repository](https://github.com/manassharma07/RIPER-Tools-for-TURBOMOLE)')
+
+
 # Title
 st.title("Density of States Plotter")
 
@@ -11,6 +35,8 @@ st.code("""
 # Add this snippet to your control file
 $dosper width=real emin=real emax=real scal=real npt=integer
 """)
+st.write('and run `riper` as')
+st.code('nohup riper -proper > dos.out &', language='shell')
 
 # Input for spin-restricted or spin-unrestricted calculation
 spin_restricted = st.checkbox("Spin-Restricted Calculation", value=True)
