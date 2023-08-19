5daf40cdc79412aa785b275298091c5d4f5c1e59
manas sharma <mikeatthetop@gmail.com>
2023-08-19 16:56:16 +0200

    Added a band structure path creator

--- pages/Band_Structure_Path.py
+++ pages/Band_Structure_Path.py
@@ -58,27 +58,25 @@ else:
 if structure:
     if use_primitive:
         primitive_structure = structure.get_primitive_structure()
-        st.write("Primitive structure:")
-        st.write(primitive_structure)
+        st.write("Converted to Primitive Structure! Using primitive structure from now on.")
         atoms = AseAtomsAdaptor.get_atoms(primitive_structure)
     else:
-        st.write("Conventional structure:")
-        st.write(structure)
+        st.write("Using Conventional Structure:")
         atoms = AseAtomsAdaptor.get_atoms(structure)
 
     lat = atoms.cell.get_bravais_lattice()
     special_points = lat.get_special_points()
 
-    st.write("Special k-points:")
+    st.write("### Special k-points:")
     st.write(special_points)
 
     bandpath = atoms.cell.bandpath()
     bandpath_str = bandpath.path
     nlines = bandpath_str.count(",") + 1
 
-    st.write("Number of lines (paths) in band structure:", nlines)
+    st.write("#### Number of lines (paths) in band structure:", nlines)
 
-    st.write("Generate TURBOMOLE band structure calculation text:")
+    st.write("### Input text for RIPER band structure calculation (Add it to your `control` file)")
     text_area_content = generate_turbomole_text(bandpath_str)
     turbomole_text = st.text_area("TURBOMOLE input text", value=text_area_content, height=200)
 
