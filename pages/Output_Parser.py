b5ff18c8c2efa0e90daa52c887f1c4dbe8a1e0c8
manas sharma <mikeatthetop@gmail.com>
2023-08-27 21:50:11 +0200

    update output parser

--- pages/Output_Parser.py
+++ pages/Output_Parser.py
@@ -3,6 +3,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from io import StringIO
+from pymatgen.core import Structure, Lattice, Element
+
+def find_line_with_text(lines, text):
+    for line in lines:
+        if text in line:
+            return line
+    return None
+
 
 def parse_energies(text):
     kinetic_energy = []
@@ -24,7 +32,7 @@ def parse_energies(text):
     return kinetic_energy, coulomb_energy, exchange_corr_energy, total_energy
 
 
-st.title("RIPER Output Parser")
+st.title("`RIPER` OUTPUT PARSER")
 
 st.write('You can either paste the output file contents below or upload the source file')
 contents = st.text_area(label='Enter the contents of the output file here', value='', placeholder='Put your text here',
@@ -59,7 +67,7 @@ if contents != '':
     df = pd.DataFrame(data)
     st.dataframe(df)
 
-    st.subheader("Energy vs SCF Iteration")
+    st.subheader("Convergence (Energy vs SCF Iteration)")
     plt.figure(figsize=(10, 6))
     plt.plot(data["SCF Iteration"], data["Total Energy"], marker='o', linestyle='-', color='b', label='Total Energy')
     plt.xlabel("SCF Iteration")
@@ -68,4 +76,54 @@ if contents != '':
     plt.legend()
     st.pyplot(plt)
 
+
+    # Find periodicity and lattice parameters
+    lines = file_contents.decode('utf-8').split('\n')
+    cell_params_line = find_line_with_text(lines, "Cell parameters (au,deg.)")
+    
+    if cell_params_line is not None:
+        fourth_line = lines[lines.index(cell_params_line) + 3]
+        num_elements = len(fourth_line.split())
+        
+        if num_elements == 6:
+            periodicity = 3
+        elif num_elements == 3:
+            periodicity = 2
+        elif num_elements == 1:
+            periodicity = 1
+            
+        lattice_lines = []
+        direct_space_line = find_line_with_text(lines, "Direct space cell vectors (au):")
+        if direct_space_line is not None:
+            lattice_lines = lines[lines.index(direct_space_line) + 1 : lines.index(direct_space_line) + periodicity + 1]
+            
+
+        # Find atomic coordinates
+        fractional_coords_line = find_line_with_text(lines, "fractional coordinates")
+        if fractional_coords_line is not None:
+            atomic_coords_lines = lines[lines.index(fractional_coords_line) + 1:]
+            atomic_coords = []
+            
+            for line in atomic_coords_lines:
+                if line.strip() == "":
+                    break  # Stop when an empty line is encountered
+                parts = line.split()
+                element = parts[0].capitalize()  # Assuming lowercase element symbols
+                coords = list(map(float, parts[1:4]))
+                atomic_coords.append((element, coords))
+
+            # Create the lattice using the lattice vectors
+            lattice_vectors = []
+            for line in lattice_lines:
+                lattice_vectors.append(list(map(float, line.split()[:3])))
+            lattice = Lattice(lattice_vectors)
+
+            # Create the sites using atomic coordinates
+            sites = []
+            for element, coords in atomic_coords:
+                site = (Element(element), coords)
+                sites.append(site)
+
+            # Create the Structure object
+            structure = Structure(lattice, sites)
 
