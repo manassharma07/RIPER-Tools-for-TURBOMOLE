a46e62d91735fa0329677f92a981197c6f294e55
manas sharma <mikeatthetop@gmail.com>
2023-08-27 18:11:19 +0200

    Prevent visualization of structures with more than 500 atoms as that may overload the server

--- pages/Convert_other_formats_to_RIPER.py
+++ pages/Convert_other_formats_to_RIPER.py
@@ -377,7 +377,13 @@ if contents != '':
         # st.write(conventional_structure)
         supercell_structure = structure.copy()
         supercell_structure.make_supercell([int(nx), int(ny), int(nz)])
-        visualize_structure(supercell_structure, 'viz2.html')
+        # Get the number of atoms
+        num_atoms_supercell = supercell_structure.num_sites
+        if num_atoms_supercell<500:
+            visualize_structure(supercell_structure, 'viz2.html')
+        else:
+            st.warning("We can't visualize your supercell as it contains more than 500 atoms which is a bit too much for a free web app.\n But don't worry, RIPER can still do the calculations with ease (provided you have the required resources).")
+
 
         # Get TURBOMOLE (RIPER) Coord file and Control file contents
         st.subheader("RIPER Files for the Supercell")
