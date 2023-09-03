31359330cd2a31d30e099105cae97334f4a552c7
manas sharma <mikeatthetop@gmail.com>
2023-09-03 08:44:35 +0200

    Update RT-TDDFT input Creator

--- pages/RT-TDDFT_Input_Creator.py
+++ pages/RT-TDDFT_Input_Creator.py
@@ -141,7 +141,7 @@ st.table(keywords_df)
 
 st.warning('## Implementation Paper')
 st.write('Please cite the following paper if you use the RT-TDDFT feature')
-citation_text = "Real-time time-dependent density functional theory using density fitting and the continuous fast multipole method\nCarolin Müller, Manas Sharma, Marek Sierka"
+citation_text = "Carolin Müller, Manas Sharma, Marek Sierka,\n*Real-time time-dependent density functional theory using density fitting and the continuous fast multipole method*,\n J Comput Chem. 2020; 41: 2573–258"
 
 # Create a hyperlink for the citation
 citation_link = "[Read the paper](https://onlinelibrary.wiley.com/doi/full/10.1002/jcc.26412)"
