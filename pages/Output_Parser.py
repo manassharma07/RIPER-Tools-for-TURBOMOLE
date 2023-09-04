29f67296c34326401d74dd34122564b16b08e260
manas sharma <mikeatthetop@gmail.com>
2023-09-04 13:46:26 +0200

    Update Output parser

--- pages/Output_Parser.py
+++ pages/Output_Parser.py
@@ -237,6 +237,7 @@ if contents != '':
                 lattice_vectors = []
                 for line in lattice_lines:
                     lattice_vectors.append(list(map(float, line.split()[1:4])))
+                lattice_vectors.append([0.0, 0.0, 50])
                 lattice = Lattice(lattice_vectors, pbc=[True, True, False])
                 lattice = lattice.matrix*0.52917721092
 
