f8eb47ea58e54680998a7c0889fafa2b53bd7cf8
Manas Sharma <manas.sharma@uni-jena.de>
2023-12-06 18:43:58 +0100

    finalize 2d band paths

--- pages/Band_Structure_Path.py
+++ pages/Band_Structure_Path.py
@@ -289,7 +289,8 @@ if structure:
         atoms = AseAtomsAdaptor.get_atoms(structure)
         display_structure_info_ase(structure, atoms)
 
-    pbc = st.selectbox('Specify PBC (EXPERIMENTAL FEATURE IN TESTING): ', ['3D', '2D'])
+    st.write("### Specify Periodicity (EXPERIMENTAL FEATURE)")
+    pbc = st.selectbox('PBC: ', ['3D', '2D'])
     if pbc=='3D':
         pbc_arr = [1,1,1]
     if pbc=='2D':
