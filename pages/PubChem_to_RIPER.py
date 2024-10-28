b944cd0ee6d32a73a4dd614c58db22da667fa31c
Manas Sharma <manas.sharma@uni-jena.de>
2024-10-28 10:16:37 +0530

    improve formatting of xyz in pubchem to riper

--- pages/PubChem_to_RIPER.py
+++ pages/PubChem_to_RIPER.py
@@ -37,7 +37,7 @@ def generate_xyz_coordinates(cid):
     coords = [(atom.x, atom.y, atom.z) for atom in atoms]
 
     num_atoms = len(atoms)
-    xyz_text = f"{num_atoms}\n{compound.cid}\n"
+    xyz_text = f"{num_atoms}\n{compound.cid}  {compound.formula}\n"
 
     for atom, coord in zip(atoms, coords):
         atom_symbol = atom.element
