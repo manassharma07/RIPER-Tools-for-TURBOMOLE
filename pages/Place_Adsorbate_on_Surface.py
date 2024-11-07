47b8775f1a11fca1e4bee8fe60445312a4d1bdd6
Manas Sharma <manas.sharma@uni-jena.de>
2024-11-07 23:26:00 +0530

    improvements to adsorbate placer

--- pages/Place_Adsorbate_on_Surface.py
+++ pages/Place_Adsorbate_on_Surface.py
@@ -3,8 +3,133 @@ import io
 from ase import Atoms
 from ase.io import read, write
 from ase.build import add_adsorbate
-# from pymatgen.io.ase import AseAtomsAdaptor
-import tempfile
+from pymatgen.io.ase import AseAtomsAdaptor
+import py3Dmol
+import pandas as pd
+import streamlit.components.v1 as components
+
+# Set page config
+st.set_page_config(page_title='Pack Molecules in Cell', layout='wide', page_icon="⚛️",
+                   menu_items={
+                       'About': "A web app to help you with DFT related calculations using the RIPER module of [TURBOMOLE](https://www.turbomole.org/)"
+                   })
+
+# Sidebar stuff
+st.sidebar.write('# About')
+st.sidebar.write('Made By [Manas Sharma](https://manas.bragitoff.com)')
+st.sidebar.write('In the group of [Prof. Dr. Marek Sierka](https://cmsg.uni-jena.de)')
+st.sidebar.write('### *Powered by*')
+st.sidebar.write('* [Py3Dmol](https://3dmol.csb.pitt.edu/) for Chemical System Visualizations')
+st.sidebar.write('* [Streamlit](https://streamlit.io/) for making of the Web App')
+st.sidebar.write('* [PyMatgen](https://pymatgen.org/) for Periodic Structure Representations')
+st.sidebar.write('* [PubChempy](https://pypi.org/project/PubChemPy/1.0/) for Accessing the PubChem Database')
+st.sidebar.write('* [MP-API](https://pypi.org/project/mp-api/) for Accessing the Materials Project Database')
+st.sidebar.write('* [ASE](https://wiki.fysik.dtu.dk/ase/) for File Format Conversions')
+st.sidebar.write('### *Contributors*')
+st.sidebar.write('[Ya-Fan Chen ](https://github.com/Lexachoc)')
+st.sidebar.write('### *Source Code*')
+st.sidebar.write('[GitHub Repository](https://github.com/manassharma07/RIPER-Tools-for-TURBOMOLE)')
+
+# Function to visualize the structure using py3Dmol
+def visualize_structure(structure, html_file_name='viz.html'):
+    spin = st.checkbox('Spin', value=False, key='key' + html_file_name)
+    view = py3Dmol.view(width=500, height=400)
+    cif_for_visualization = structure.to(fmt="cif")
+    view.addModel(cif_for_visualization, 'cif')
+    # view.setStyle({'stick': {'radius': 0.2}})
+    view.setStyle({'sphere': {'colorscheme': 'Jmol', 'scale': 0.3},
+                   'stick': {'colorscheme': 'Jmol', 'radius': 0.2}})
+    view.addUnitCell()
+    view.zoomTo()
+    view.spin(spin)
+    view.setClickable({'clickable': 'true'})
+    view.enableContextMenu({'contextMenuEnabled': 'true'})
+    view.show()
+    view.render()
+    # view.png()
+    t = view.js()
+    f = open(html_file_name, 'w')
+    f.write(t.startjs)
+    f.write(t.endjs)
+    f.close()
+
+    HtmlFile = open(html_file_name, 'r', encoding='utf-8')
+    source_code = HtmlFile.read()
+    components.html(source_code, height=300, width=500)
+    HtmlFile.close()
+
+
+# Function to visualize the structure using py3Dmol
+def visualize_molecule(structure, html_file_name='viz.html'):
+    spin = st.checkbox('Spin', value=False, key='key' + html_file_name)
+    view = py3Dmol.view(width=500, height=400)
+    cif_for_visualization = structure.to(fmt="xyz")
+    view.addModel(cif_for_visualization, 'xyz')
+    # view.setStyle({'stick': {'radius': 0.2}})
+    view.setStyle({'sphere': {'colorscheme': 'Jmol', 'scale': 0.3},
+                   'stick': {'colorscheme': 'Jmol', 'radius': 0.2}})
+    view.zoomTo()
+    view.spin(spin)
+    view.setClickable({'clickable': 'true'})
+    view.enableContextMenu({'contextMenuEnabled': 'true'})
+    view.show()
+    view.render()
+    # view.png()
+    t = view.js()
+    f = open(html_file_name, 'w')
+    f.write(t.startjs)
+    f.write(t.endjs)
+    f.close()
+
+    HtmlFile = open(html_file_name, 'r', encoding='utf-8')
+    source_code = HtmlFile.read()
+    components.html(source_code, height=300, width=500)
+    HtmlFile.close()
+
+
+# Function to display structure information
+@st.fragment
+def display_structure_info(structure):
+    st.subheader("Structure Information")
+    st.write("Formula: ", structure.composition.reduced_formula)
+
+    # Display lattice parameters
+    a, b, c = structure.lattice.abc
+    alpha, beta, gamma = structure.lattice.angles
+
+    # Create a DataFrame for the lattice parameters and angles
+    data = {
+        "Lattice Parameters": [a, b, c, alpha, beta, gamma]
+    }
+    df_latt_params = pd.DataFrame(data, index=["a", "b", "c", "alpha", "beta", "gamma"])
+    with st.expander("Lattice Parameters", expanded=False):
+        st.table(df_latt_params)
+
+    # Display lattice vectors
+    lattice_vectors = structure.lattice.matrix
+    df_vectors = pd.DataFrame(lattice_vectors, columns=["X", "Y", "Z"], index=["a", "b", "c"])
+    with st.expander("Lattice Vectors", expanded=True):
+        # st.write("Lattice Vectors:")
+        st.table(df_vectors)
+
+    # Create a list of atomic coordinates
+    with st.expander("Atomic Coordinates", expanded=False):
+        coord_type = st.selectbox('Coordinate type', ['Cartesian', 'Fractional/Crystal'], key='selectbox'+structure.composition.reduced_formula)
+        if coord_type == 'Cartesian':
+            atomic_coords = []
+            for site in structure.sites:
+                atomic_coords.append([site.species_string] + list(site.coords))
+        else:
+            atomic_coords = []
+            for site in structure.sites:
+                atomic_coords.append([site.species_string] + list(site.frac_coords))
+
+        # Create a Pandas DataFrame from the atomic coordinates list
+        df_coords = pd.DataFrame(atomic_coords, columns=["Element", "X", "Y", "Z"])
+
+        # Display the atomic coordinates as a table
+        # st.write("Atomic Coordinates:")
+        st.table(df_coords)
 
 st.title("Place Adsorbate (molecule) on a Surface (periodic)")
 st.write("1. The tool works as follows.")
@@ -103,22 +228,12 @@ if base_structure is not None and molecule is not None:
                           translate_z * base_structure.cell[2])
 
     # Add adsorbate onto the surface at a specified height
-    adsorbate_height = st.slider("Adsorbate Height (Å)", min_value=1.0, max_value=10.0, value=2.0, step=0.1)
-    # add_adsorbate(base_structure, molecule, adsorbate_height, position=(translate_x * base_structure.cell[0][0:1] + translate_y * base_structure.cell[1][0:1]))
+    adsorbate_height = st.slider("Adsorbate Height (Å)", min_value=1.0, max_value=15.0, value=2.0, step=0.1)
     add_adsorbate(base_structure, molecule, adsorbate_height, position=adsorbate_position[:2])
     
     # Display packed structure info
     st.subheader("Structure Preview and Download")
     st.write("The molecule has been positioned on the surface. You can download the final structure as a CIF file.")
     
-    # Download button
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".cif") as tmp_file:
-        write(tmp_file.name, base_structure, format="cif")
-        tmp_file_path = tmp_file.name
-    with open(tmp_file_path, "rb") as file:
-        btn = st.download_button(
-            label="Download Packed Structure as CIF",
-            data=file,
-            file_name="packed_structure.cif",
-            mime="chemical/x-cif"
-        )
+    # Download option
+    download_packed_struture(base_structure)
