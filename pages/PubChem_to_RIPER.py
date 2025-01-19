7fe76801eae7d7265515b7c800e5507c71cb6be2
Manas Sharma <manas.sharma@uni-jena.de>
2025-01-19 14:21:20 +0530

    switch back to older small model for mace optimizer in pubcchem to riper

--- pages/PubChem_to_RIPER.py
+++ pages/PubChem_to_RIPER.py
@@ -150,7 +150,8 @@ def format_coord(molecule):
 
 @st.cache_resource
 def get_mace_mp():
-    return mace_mp(model="https://github.com/ACEsuit/mace-mp/releases/download/mace_omat_0/mace-omat-0-medium.model", device="cpu", default_dtype="float32")
+    # return mace_mp(model="https://github.com/ACEsuit/mace-mp/releases/download/mace_omat_0/mace-omat-0-medium.model", device="cpu", default_dtype="float32")
+    return mace_mp(model="small", device="cpu", default_dtype="float32")
 # device = "cpu" #"cuda"
 # Initialize the MACE-MP calculator
 # mace_mp_calc = mace_mp(model="small", device=device, default_dtype="float32")
