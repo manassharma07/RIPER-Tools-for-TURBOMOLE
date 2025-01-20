b02b273656d08f33fce6a563016ef0234fa802a3
Manas Sharma <manas.sharma@uni-jena.de>
2025-01-20 19:09:46 +0530

    debugging adsorbate placer

--- pages/Place_Adsorbate_on_Surface.py
+++ pages/Place_Adsorbate_on_Surface.py
@@ -299,7 +299,7 @@ if base_structure is not None and molecule is not None:
 
     # Add adsorbate onto the surface at a specified height
     adsorbate_height = col2.slider("Adsorbate Height (Å)", min_value=-10.0, max_value=15.0, value=2.0, step=0.1)
-    add_adsorbate(base_structure, molecule, adsorbate_height, position=adsorbate_position[:2])
+    add_adsorbate(base_structure, molecule, adsorbate_height, position=(0,0))
     packed_structure_pymatgen = AseAtomsAdaptor().get_structure(base_structure)
     
     col1.subheader("Structure Preview and Download")
